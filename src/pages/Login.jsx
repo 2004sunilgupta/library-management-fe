94c228fb1237a6fe8a7844131753e93e62dd6151
2004sunilgupta <2004sunilgupta@gmail.com>
2024-08-25 18:10:02 +0530

    Login Validation handle

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,12 +2,18 @@ import { useEffect, useState } from "react";
 import { authService } from "../services/authService";
 import { useNavigate } from "react-router-dom";
 import { routerPath } from "../constants/routerConstant";
+import FormInput from "../components/FormInput";
+import { validateEmail } from "../utils/validation";
 
 const Login = () => {
 
     const [email, setEmail] = useState('');
     const [passowrd, setPassword] = useState('');
-    const [error, setError] = useState('');
+    const [error, setError] = useState({
+        emailErr: '',
+        passwordErr: '',
+        errMsg: ''
+    });
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -21,9 +27,34 @@ const Login = () => {
             const res = await authService.login(email, passowrd);
             console.log(res);
             navigate(routerPath.CREATE_USER);
-        } catch (error) {
-            setError(error.message);
+        } catch (err) {
+            console.log(err.message);
+            setError({...error, errMsg: "Email or Password didn't matched"});
         }
+    }
+
+    const handleEmailBlur = (value) => {
+        if (!value) {
+            setError({...error, emailErr: 'Please enter vemail'});
+            return;
+        };
+        if(!validateEmail(email)) {
+            setError({...error, emailErr: 'Please enter valid email'});
+        } else {
+            setError({...error, emailErr: ''});
+        }
+    }
+
+    const handlePasswordBlur = (value) => {
+        if (!value) {
+            setError({...error, passwordErr: 'Please enter password'});
+        } else {
+            setError({...error, passwordErr: ''});
+        }
+    }
+    
+    const handleDisabled = () => {
+        return error.emailErr.length || error.passwordErr.length || error.errMsg.length;
     }
 
 
@@ -31,14 +62,12 @@ const Login = () => {
         <div className="page-login">
             <div className="container">
                 <h1>Login Page</h1>
-                <div className="form-group mb-3">
-                    <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                </div>
-                <div className="form-group mb-3">
-                    <input type="password" className="form-control" value={passowrd} onChange={(e) => setPassword(e.target.value)} required />
-                </div>
-                {error && <p>{error}</p>}
-                <button type="button" onClick={handleLogin} className="btn btn-success">Login</button>
+                <FormInput type="email" label={'Email'} value={email} handleChange={(e) => setEmail(e.target.value)} required={true}
+                validationMessage={error.emailErr} handleOnBlur={()=> handleEmailBlur(email)}/>
+                <FormInput type="password" label={'Password'} value={passowrd} handleChange={(e) => setPassword(e.target.value)} required={true}
+                validationMessage={error.passwordErr} handleOnBlur={()=> handlePasswordBlur(passowrd)}/>
+                {error.errMsg && <p className="text-danger">{error.errMsg}</p>}
+                <button type="button" onClick={handleLogin} className="btn btn-success" disabled={error.emailErr.length || error.passwordErr.length || error.errMsg.length}>Login</button>
             </div>
         </div>
     );
